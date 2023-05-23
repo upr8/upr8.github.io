b5416da4b0c3b8e8a2fb5b9b9b000d7191993e84
upr8 <134240395+upr8@users.noreply.github.com>
2023-05-23 10:49:40 +0000

    [style]: add theme for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
 		"./src/components/**/*.{js,jsx,ts,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				body: "var(--font-body)",
+			},
+			backgroundColor: {
+				body: "var(--color-bg-body)",
+				nav: "var(--color-bg-nav)",
+				bq: "var(--color-bg-bq)",
+			},
+			borderColor: {
+				primary: "var(--color-border-primary)",
+				secondary: "var(--color-border-secondary)",
+			},
+			textColor: {
+				primary: "var(--color-text-primary)",
+				secondary: "var(--color-text-secondary)",
+			},
+		},
 	},
 	plugins: [],
 };
