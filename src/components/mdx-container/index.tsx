c8a357ed8ebad8417befabe4b6035b33bd258b59
upr8 <134240395+upr8@users.noreply.github.com>
2023-05-23 10:50:42 +0000

    [style]: add style for mdx posts

--- src/components/mdx-container/index.tsx
+++ src/components/mdx-container/index.tsx
@@ -2,13 +2,19 @@ import { MDXProvider } from "@mdx-js/react";
 import { Link } from "gatsby";
 import * as React from "react";
 
+import { postBody } from "./mdx.module.css";
+
 interface Props {
 	children: React.ReactNode;
 }
 const shortcodes = { Link };
 
 const MdxContainer: React.FC<Props> = ({ children }) => {
-	return <MDXProvider components={shortcodes}>{children}</MDXProvider>;
+	return (
+		<div className={postBody}>
+			<MDXProvider components={shortcodes}>{children}</MDXProvider>
+		</div>
+	);
 };
 
 export default MdxContainer;
