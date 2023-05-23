a64cdafbc331d402a4c580ab64b296df4f1353c1
upr8 <134240395+upr8@users.noreply.github.com>
2023-05-23 08:57:07 +0000

    [react]: add template for about page

---
+++ src/templates/about.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { graphql, PageProps } from "gatsby";
+import MdxContainer from "../components/mdx-container";
+import Layout from "../components/layout";
+
+interface Props extends PageProps {
+	data: Queries.Query;
+}
+
+const AboutMeTemplate: React.FC<Props> = ({ data }) => {
+	const post = data.mdx;
+
+	return (
+		<Layout>
+			<MdxContainer>{post?.body}</MdxContainer>
+		</Layout>
+	);
+};
+
+export default AboutMeTemplate;
+
+export const query = graphql`
+    query ($slug: String!) {
+        mdx(fields: { slug: { eq: $slug } }) {
+            body
+            frontmatter {
+                title
+                date(formatString: "DD MMMM, YYYY")
+                lang
+                desc
+            }
+            fields {
+                slug
+            }
+        }
+    }
+`;
