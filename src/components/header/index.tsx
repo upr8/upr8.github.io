f01d706dd832fd35ac3a84d59de80acd8bc943cf
upr8 <134240395+upr8@users.noreply.github.com>
2023-05-24 00:04:18 +0000

    [react]: use context to change the state of theme with a button in header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import { SiteContext, toggleTheme } from "../../states";
 
 const Header: React.FC<{}> = () => {
+	const { dispatch } = React.useContext(SiteContext);
+
 	return (
 		<header className="mb-32">
 			<div className="box-border flex items-center justify-between min-w-0 m-0">
 				<div className="my-0 text-3xl font-semibold">Saeed Asaiyan</div>
-				<button type="button">DarkMode</button>
+				<button type="button" onClick={() => dispatch(toggleTheme())}>
+					DarkMode
+				</button>
 			</div>
 			<div className="box-border flex flex-wrap items-center justify-between pb-4 mt-4 border-b-2">
 				<nav className="text-lg">
