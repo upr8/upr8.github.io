be81e6eaa184ae567bfa4fb27cc182004217d81c
upr8 <134240395+upr8@users.noreply.github.com>
2023-05-25 06:13:17 +0000

    [gatsby]: refactor gatsby-head to use getDirectionFromLang

--- src/components/gatsby-head/index.tsx
+++ src/components/gatsby-head/index.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import { HeadFC } from "gatsby";
 import { PageContext } from "../../gatsby/types";
-import { Language } from "../../states";
+import { getDirectionFromLang } from "../../utils";
 
 export const Head: HeadFC<{}, PageContext> = ({
 	pageContext: { lang, title },
 }) => {
 	return (
 		<>
-			<html lang={lang} dir={lang === Language.English ? "ltr" : "rtl"} />
+			<html lang={lang} dir={getDirectionFromLang(lang)} />
 			<title>{title}</title>
 		</>
 	);
