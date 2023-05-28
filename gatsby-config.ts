afb119b917d658d05fc228912412dcc1d07577d5
upr8 <134240395+upr8@users.noreply.github.com>
2023-05-28 08:26:48 +0000

    [gatsby]: fix siteUrl in siteMetadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,7 +4,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
 	siteMetadata: {
 		title: "",
-		siteUrl: "https://www.yourdomain.tld",
+		siteUrl: "https://www.asaiyan.com",
 	},
 	// More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
 	// If you use VSCode you can also use the GraphQL plugin
