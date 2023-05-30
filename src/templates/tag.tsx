9fe5d0224780bbf475a8dabf66cfa7dc049a7f4f
upr8 <134240395+upr8@users.noreply.github.com>
2023-05-30 07:54:51 +0000

    [react]: complete the tag template page

--- src/templates/tag.tsx
+++ src/templates/tag.tsx
@@ -2,11 +2,14 @@ import * as React from "react";
 import { PageProps, graphql } from "gatsby";
 import Layout from "@/components/layout";
 import { PageContext } from "@/gatsby/types";
+import BlogCard from "@/components/cards/blog-post-card";
+import { ArchivePostCard } from "@/components/cards";
+import { LibraryBookCard } from "@/components/cards";
 
 export { Head } from "@/components/gatsby-head";
 
 interface Props extends PageProps {
-	data: Queries.Query;
+	data: Queries.TagPageQuery;
 	pageContext: PageContext;
 }
 
@@ -15,15 +18,44 @@ const TagIndexTemplate: React.FC<Props> = ({ data, pageContext }) => {
 		<Layout pageContext={pageContext}>
 			<article>
 				<section>
-					{data.allMdx.edges.map(
-						({ node }): React.JSX.Element => (
-							<ol key={node.id}>
-								<li>{node.frontmatter?.title}</li>
-								<li>{node.frontmatter?.desc}</li>
-								<li>{node.frontmatter?.date}</li>
-							</ol>
-						),
-					)}
+					<h4 className="mt-8 text-xl italic font-semibold text-secondary">
+						{data.blog.totalCount > 0 ? (
+							<>Blog posts:</>
+						) : (
+							<>There is no blog post with this tag</>
+						)}
+					</h4>
+					{data.blog.edges.map(({ node }) => (
+						<BlogCard key={node.id} BlogNode={node} />
+					))}
+				</section>
+				<hr className="h-px mx-4 my-8 bg-gray-300 border-0 dark:bg-gray-600" />
+				<section>
+					<h4 className="mt-8 text-xl italic font-semibold text-secondary">
+						{data.library.totalCount > 0 ? (
+							<>Books in my library:</>
+						) : (
+							<>There is no books in my library with this tag</>
+						)}
+					</h4>
+					<div className="flex flex-wrap justify-around">
+						{data.library.edges.map(({ node }) => (
+							<LibraryBookCard key={node.id} BookNode={node} />
+						))}
+					</div>
+				</section>
+				<hr className="h-px mx-4 my-8 bg-gray-300 border-0 dark:bg-gray-600" />
+				<section>
+					<h4 className="mt-8 text-xl italic font-semibold text-secondary">
+						{data.archive.totalCount > 0 ? (
+							<>Links in archive:</>
+						) : (
+							<>There is no link in my archive with this tag</>
+						)}
+					</h4>
+					{data.archive.edges.map(({ node }) => (
+						<ArchivePostCard key={node.id} ArchiveNode={node} />
+					))}
 				</section>
 			</article>
 		</Layout>
@@ -33,8 +65,8 @@ const TagIndexTemplate: React.FC<Props> = ({ data, pageContext }) => {
 export default TagIndexTemplate;
 
 export const query = graphql`
-    query ($tag: String!) {
-        allMdx(
+    query TagPage($tag: String!) {
+        blog: allMdx(
             filter: {
                 frontmatter: {
                     tags: { in: [$tag] }
@@ -47,7 +79,41 @@ export const query = graphql`
             totalCount
             edges {
                 node {
-                    body
+                    ...BlogNode
+                }
+            }
+        }
+        library: allMdx(
+            filter: {
+                frontmatter: {
+                    tags: { in: [$tag] }
+                    published: { eq: true }
+                    type: { eq: "book" }
+                }
+            }
+            sort: {frontmatter: {date: DESC}}
+        ) {
+            totalCount
+            edges {
+                node {
+                    ...BookNode
+                }
+            }
+        }
+        archive: allMdx(
+            filter: {
+                frontmatter: {
+                    tags: { in: [$tag] }
+                    published: { eq: true }
+                    type: { eq: "archive" }
+                }
+            }
+            sort: {frontmatter: {date: DESC}}
+        ) {
+            totalCount
+            edges {
+                node {
+                    ...ArchiveNode
                 }
             }
         }
