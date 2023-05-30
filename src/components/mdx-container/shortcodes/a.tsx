7108a501a34ecb74a00c20a99c4ccd935acd491f
upr8 <134240395+upr8@users.noreply.github.com>
2023-05-30 01:39:48 +0000

    [react]: fix target attribute of links that stats with #

--- src/components/mdx-container/shortcodes/a.tsx
+++ src/components/mdx-container/shortcodes/a.tsx
@@ -4,14 +4,16 @@ import * as React from "react";
 interface Props {
 	href: string;
 	title?: string;
+	className?: string;
 	children: React.ReactElement;
 }
 
-const A = ({ href, title, children }: Props) => {
+const A = ({ href, title, className, children }: Props) => {
 	const { siteUrl } = useSiteMetadata();
-	const target = href.startsWith(siteUrl) ? "_self" : "_blank";
+	const target =
+		href.startsWith(siteUrl) || href.startsWith("#") ? "_self" : "_blank";
 	return (
-		<a href={href} title={title} target={target}>
+		<a href={href} title={title} target={target} className={className}>
 			{children}
 		</a>
 	);
