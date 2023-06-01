c7c986b37953d1b8c6afc80aa0d79d45dd1ad802
upr8 <134240395+upr8@users.noreply.github.com>
2023-06-01 12:04:44 +0000

    [gatsby]: remove the annoying popup in onServiceWorkerUpdateReady

--- src/gatsby/on-service-worker-update-ready.ts
+++ src/gatsby/on-service-worker-update-ready.ts
@@ -1,9 +1,3 @@
 export const onServiceWorkerUpdateReady = () => {
-	const answer = window.confirm(
-		"This application has been updated. Reload to display the latest version?",
-	);
-
-	if (answer === true) {
-		window.location.reload();
-	}
+	window.location.reload();
 };
