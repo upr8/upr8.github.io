72f1b345f0087a5a6f7b7db6f2e9789431de90b5
upr8 <134240395+upr8@users.noreply.github.com>
2023-06-01 00:51:06 +0000

    [gatsby]: add google-analytics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,10 @@
 import { Edge } from "@/gatsby/types";
 import type { GatsbyConfig } from "gatsby";
 
+require("dotenv").config({
+	path: ".env",
+});
+
 const config: GatsbyConfig = {
 	// flags: {
 	// 	DEV_SSR: true, //https://www.gatsbyjs.com/docs/debugging-html-builds/#ssr-during-gatsby-develop
@@ -14,6 +18,28 @@ const config: GatsbyConfig = {
 	// Learn more at: https://gatsby.dev/graphql-typegen
 	graphqlTypegen: true,
 	plugins: [
+		{
+			resolve: "gatsby-plugin-google-gtag",
+			options: {
+				// You can add multiple tracking ids and a pageView event will be fired for all of them.
+				trackingIds: [process.env.GTAG],
+				// This object gets passed directly to the gtag config command
+				// This config will be shared across all trackingIds
+				gtagConfig: {
+					anonymize_ip: true,
+					cookie_expires: 0,
+				},
+				// This object is used for configuration specific to this plugin
+				pluginConfig: {
+					// Puts tracking script in the head instead of the body
+					head: true,
+					// Setting this parameter is also optional
+					respectDNT: true,
+					// Avoids sending pageView hits from custom paths
+					exclude: ["/preview/**", "/do-not-track/me/too/"],
+				},
+			},
+		},
 		"gatsby-plugin-postcss",
 		"gatsby-plugin-image",
 		"gatsby-plugin-sharp",
