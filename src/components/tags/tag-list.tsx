b58c006d3340a472e2326ad289cb44bee98c00f7
upr8 <134240395+upr8@users.noreply.github.com>
2025-11-19 22:47:44 +1100

    refactor: upgrade the tailwind to v4

--- src/components/tags/tag-list.tsx
+++ src/components/tags/tag-list.tsx
@@ -19,7 +19,7 @@ const TagList: FC<Props> = ({ tags, isCenter = true }) => {
 			{tags?.map((tag) => (
 				<li
 					key={tag?.tag}
-					className="z-10 inline-block px-2 m-1 text-sm border-2 rounded-full shadow-sm bg-nav text-primary"
+					className="z-10 inline-block px-2 m-1 text-sm border-2 rounded-full shadow-xs bg-nav text-primary"
 				>
 					<Link to={`${tag?.slug}`}>#{tag?.tag}</Link>
 				</li>
