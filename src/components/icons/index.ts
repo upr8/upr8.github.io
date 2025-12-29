90642abf70d3de75382b54ac0ec7a7b7153a3651
upr8 <134240395+upr8@users.noreply.github.com>
2025-12-30 01:20:33 +1100

    feat: add sitemap to the footer

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -3,3 +3,4 @@ export { default as Linkedin } from "./linkedin";
 export { default as Github } from "./github";
 export { default as Menu } from "./menu";
 export { default as Close } from "./close";
+export { default as Rss } from "./rss";
