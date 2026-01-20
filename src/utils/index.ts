2187971fc4faa16b6c85660d801aba3ae10a2a16
upr8 <134240395+upr8@users.noreply.github.com>
2026-01-21 09:41:28 +1100

    feat: redesign home page

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,5 @@ export { getDirectionFromLang } from "./dir";
 export { getLocaleFromLang } from "./locale";
 export { getLocalStorage } from "./get-localstorage";
 export { setLocalStorage } from "./set-localstorage";
+export { getSlug, getLatestPosts, getLatestBooks, getTagData } from "./content";
+export type { TagData } from "./content";
