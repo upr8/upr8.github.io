82b54821cf776676b71288155e61666cdb635e33
Saeed <134240395+upr8@users.noreply.github.com>
2026-07-30 22:42:03 +1000

    deps: upgrade the dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import { unified } from '@astrojs/markdown-remark';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import AstroPWA from '@vite-pwa/astro';
@@ -18,7 +19,7 @@ export default defineConfig({
   site: 'https://www.asaiyan.com',
   output: 'static',
   integrations: [
-    mdx({ remarkPlugins: [remarkGfm] }),
+    mdx(),
     sitemap({
       filter: (page) => !page.endsWith('.mdx'),
     }),
@@ -60,50 +61,52 @@ export default defineConfig({
     }),
   ],
   markdown: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [
-      rehypeSlug,
-      [rehypeAutolinkHeadings, {
-        behavior: 'append',
-        properties: {
-          className: ['autolink-hdrs'],
-          ariaHidden: true,
-          tabIndex: -1,
-        },
-        content: {
-          type: 'element',
-          tagName: 'span',
-          properties: {},
-          children: [{
+    processor: unified({
+      remarkPlugins: [remarkGfm],
+      rehypePlugins: [
+        rehypeSlug,
+        [rehypeAutolinkHeadings, {
+          behavior: 'append',
+          properties: {
+            className: ['autolink-hdrs'],
+            ariaHidden: true,
+            tabIndex: -1,
+          },
+          content: {
             type: 'element',
-            tagName: 'svg',
-            properties: {
-              xmlns: 'http://www.w3.org/2000/svg',
-              width: 16,
-              height: 16,
-              fill: 'currentColor',
-              viewBox: '0 0 16 16',
-            },
+            tagName: 'span',
+            properties: {},
             children: [{
               type: 'element',
-              tagName: 'path',
+              tagName: 'svg',
               properties: {
-                d: 'M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z',
+                xmlns: 'http://www.w3.org/2000/svg',
+                width: 16,
+                height: 16,
+                fill: 'currentColor',
+                viewBox: '0 0 16 16',
               },
-              children: [],
-            }, {
-              type: 'element',
-              tagName: 'path',
-              properties: {
-                d: 'M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z',
-              },
-              children: [],
+              children: [{
+                type: 'element',
+                tagName: 'path',
+                properties: {
+                  d: 'M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z',
+                },
+                children: [],
+              }, {
+                type: 'element',
+                tagName: 'path',
+                properties: {
+                  d: 'M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z',
+                },
+                children: [],
+              }],
             }],
-          }],
-        },
-      }],
-      [rehypeExternalLinks, { target: '_blank', rel: ['noreferrer'] }],
-    ],
+          },
+        }],
+        [rehypeExternalLinks, { target: '_blank', rel: ['noreferrer'] }],
+      ],
+    }),
     shikiConfig: {
       themes: {
         light: 'github-light',
